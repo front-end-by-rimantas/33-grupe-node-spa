768f5475208fc184c9ea6a59978d28e5143448a5
Rimantas <belauzas@yahoo.com>
2022-04-25 16:19:40 +0300

    Initial server logic with demo response

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('labas');
+import { server } from "./lib/server.js";
+
+const app = {};
+
+app.init = () => {
+    // pasiruosti pradinius folderius
+    // pasiruosti pradinius failus
+    // prisijungti prie DB
+    // pasileisti serverio logika (jai duoti prieiga prie DB)
+    server.init();
+
+    // reguliariu procesu paleidimas
+}
+
+app.init();
+
+export { app };
