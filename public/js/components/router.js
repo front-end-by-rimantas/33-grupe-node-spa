ce73868bdb6207441a451417b0cc825cdc92a5ed
Rimantas <belauzas@yahoo.com>
2022-04-28 15:56:46 +0300

    Router object

--- public/js/components/router.js
+++ public/js/components/router.js
@@ -1,3 +1,28 @@
 const router = {};
 
+router.routes = {};
+
+router.notFoundRoute = null;
+
+router.formatedURL = (link) => {
+    let start = 0;
+    let end = link.length;
+    if (link[0] === '/') {
+        start = 1;
+    }
+    if (link[end - 1] === '/') {
+        end = -1;
+    }
+    return link.slice(start, end);
+}
+
+router.addRoute = (link, handler) => {
+    const formatedLink = router.formatedURL(link);
+    router.routes[formatedLink] = handler;
+}
+
+router.init = () => {
+    console.log('inicijuojamas routeris...');
+}
+
 export { router }
