11a042c25837c6dba1a73c167b9c6b99ef9f517a
Rimantas <belauzas@yahoo.com>
2022-05-09 11:16:21 +0300

    API validation: first step

--- api/account.js
+++ api/account.js
@@ -1,3 +1,5 @@
+import { IsValid } from "../lib/IsValid.js";
+
 const handler = {}
 
 handler.account = async (data, callback) => {
@@ -8,8 +10,7 @@ handler.account = async (data, callback) => {
     }
 
     return callback(400, {
-        msg: `Account API endpoint nepalaiko "${data.httpMethod
-            }" http metodo`
+        msg: `Account API endpoint nepalaiko "${data.httpMethod}" http metodo`,
     });
 }
 
@@ -22,6 +23,57 @@ handler._method.get = (data, callback) => {
 }
 
 handler._method.post = (data, callback) => {
+    const requiredKeys = ['username', 'email', 'password'];
+    const { payload } = data;
+
+    // viso objekto struktura
+    if (!payload) {
+        return callback(400, {
+            msg: 'Nevalidus JSON',
+        });
+    }
+
+    const keys = Object.keys(payload);
+    if (keys.length !== requiredKeys.length) {
+        return callback(400, {
+            msg: 'Atejes objektas nesutampa su privaloma struktura: email, password, username',
+        });
+    }
+    for (const key of keys) {
+        if (!requiredKeys.includes(key)) {
+            return callback(400, {
+                msg: `Atejes objektas turi raktazodi, kurio nereikia "${key}"`,
+            });
+        }
+    }
+
+    // tipai ir logine reiksme
+    const [usernameErr, usernameMsg] = IsValid.username(payload.username);
+    if (usernameErr) {
+        return callback(400, {
+            msg: usernameMsg,
+        });
+    }
+
+    const [emailErr, emailMsg] = IsValid.email(payload.email);
+    if (emailErr) {
+        return callback(400, {
+            msg: emailMsg,
+        });
+    }
+
+    const [passwordErr, passwordMsg] = IsValid.password(payload.password);
+    if (passwordErr) {
+        return callback(400, {
+            msg: passwordMsg,
+        });
+    }
+
+    // patikrinti, ar toks vartotojas jau registruotas (email)
+    // slaptazodzio hash'inimas
+    // irasome gautus duomenis (aka - uzregistruojame) JSON formatu (failas)
+
+    // graziname pranesima apie sekminga registracija
     return callback(200, {
         msg: 'PASKYRA SUKURTA',
     });
